ddf269cff67a80e81cb2178b90e795e45a537318
davidbte06 <davidbte06@gmail.com>
2023-09-29 16:02:49 -0600

    fixing validation and handling errors

--- controllers/consoles.js
+++ controllers/consoles.js
@@ -2,22 +2,71 @@ const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
-    const result = await mongodb.getDatabase().db().collection('consoles').find();
-    result.toArray().then((consoles) => {
+    try {
+        //#swagger.tags=['Consoles']
+        // Retrieve all consoles from the database
+        const result = await mongodb.getDatabase().db().collection('consoles').find();
+
+        // Convert the result to an array
+        const consoles = await result.toArray();
+
+        // Respond with the data
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(consoles);
-    });
+    } catch (error) {
+        // Handle errors
+        console.error(error);
+
+        if (error instanceof CustomValidationError) {
+            res.status(400).json({ error: error.message });
+        } else if (error instanceof DatabaseError) {
+            res.status(500).json({ error: 'Database error' });
+        } else {
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    }
 };
 
 const getSingle = async (req, res) => {
-    //#swagger.tags=['Consoles']
-    const consolesId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('consoles').find({ _id: consolesId });
-    result.toArray().then((consoles) => {
+    try {
+        //#swagger.tags=['Consoles']
+        const consolesId = new ObjectId(req.params.id);
+
+        // Add validation to check if req.params.id is a valid ObjectId
+        if (!ObjectId.isValid(req.params.id)) {
+            res.status(400).json({ error: 'Invalid ObjectId' });
+            return;
+        }
+
+        // Retrieve a single console by _id from the database
+        const result = await mongodb.getDatabase().db().collection('consoles').find({ _id: consolesId });
+
+        // Convert the result to an array
+        const consoles = await result.toArray();
+
+        // Check if a console with the given _id exists
+        if (consoles.length === 0) {
+            res.status(404).json({ error: 'Console not found' });
+            return;
+        }
+
+        // Respond with the data
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(consoles[0]);
-    });
+    } catch (error) {
+        // Handle errors
+        console.error(error);
+
+        if (error instanceof CustomValidationError) {
+            res.status(400).json({ error: error.message });
+        } else if (error instanceof DatabaseError) {
+            res.status(500).json({ error: 'Database error' });
+        } else {
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    }
 };
+
 
 const createConsoles = async (req, res) => {
     //#swagger.tags=['Consoles']
